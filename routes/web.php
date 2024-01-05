0ff975a6686c94524ec48c147e2c17107fdb90ad
Hongsreng-P <Hongsreng@mango-byte.com>
2024-01-05 15:01:14 +0700

    dfdsf

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::get('/files', function () {
 });
 
 Route::get('/download/{path}', function ($path) {
-    $path = "/mnt/volume_sgp1_02" . $path;
+    $path = "/mnt/volume_sgp1_02/" . $path;
     // dd($path);
 return response(Storage::disk('block_storage')->get($path))
     ->header('Content-Type', 'your/mime-type')
